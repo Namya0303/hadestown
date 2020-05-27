91a693cc3f0bbb1b3521d725f16c2f1f181275b0
Namya0303 <35605470+Namya0303@users.noreply.github.com>
2020-05-27 19:38:37 +0530

    Update update.js

--- update.js
+++ update.js
@@ -3,6 +3,6 @@
     var comments = dbRoot.ref('comments/');
     comments.on('child_added', snap => {
         var publishedOn = Date(snap.child("time").val());
-        $('.comments').prepend('<a class="card mb-3 text-left text-white bg-secondary fromtop" target="_blank"><div class="row no-gutters"><div class="col"><div class="card-body"><h5 class="card-title">' + snap.child("name").val() + '</h5><p class="card-text">' + snap.child("comment").val()'</span></small></p></div></div></div></a>');
+        $('.comments').prepend('<a class="card mb-3 text-left text-white bg-secondary fromtop" target="_blank"><div class="row no-gutters"><div class="col"><div class="card-body"><h5 class="card-title">' + snap.child("name").val() + '</h5><p class="card-text">' + snap.child("comment").val()'</span></small></p></div></div></div></a>'));
     });
 }());
